571a965a1b87827692e4c95351e32b6cc3f8e2f0
Dmitriy Koluzayev <tornadomg.team@gmail.com>
2025-08-08 23:07:30 +0500

    добавлены аннотации типов

--- src/masks.py
+++ src/masks.py
@@ -1,22 +1,16 @@
-def get_mask_card_number(card_number):
+def get_mask_card_number(card_number: str) -> str:
     """принимает на вход номер карты и возвращает ее маску"""
     try:
         masked_card = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
         return masked_card
     except Exception:
-        raise TypeError('Неверный формат аргумента')
+        raise TypeError("Неверный формат аргумента")
 
 
-
-
-def get_mask_account(account_number):
+def get_mask_account(account_number: str) -> str:
     """принимает на вход номер счета и возвращает его маску"""
     try:
         masked_account = f"**{account_number[-4:]}"
         return masked_account
     except Exception:
-        raise TypeError('Неверный формат аргумента')
-
-
-
-
+        raise TypeError("Неверный формат аргумента")
--- src/widget.py
+++ src/widget.py
@@ -1,35 +1,29 @@
-from src.masks import get_mask_card_number, get_mask_account
 from datetime import datetime
 
+from src.masks import get_mask_account, get_mask_card_number
 
-def mask_account_card(number):
-    '''обрабатывает информацию как о картах, так и о счетах'''
+
+def mask_account_card(number: str) -> str:
+    """обрабатывает информацию как о картах, так и о счетах"""
     list_number = number.split()
-    if 'Счет' == list_number[0]:
+    if "Счет" == list_number[0]:
         mask = get_mask_account(list_number[-1])
-        return f'Счет {mask}'
+        return f"Счет {mask}"
     else:
         mask = get_mask_card_number(list_number[-1])
-        return f'{' '.join(list_number[:-1])} {mask}'
-
-#print(mask_account_card(input()))
+        return f"{' '.join(list_number[:-1])} {mask}"
 
 
-def get_date(date):
-    '''возвращает даут из формата ISO'''
+# print(mask_account_card(input()))
+
+
+def get_date(date: str) -> str:
+    """возвращает дату из формата ISO"""
     # Преобразуем в объект datetime
     dt = datetime.fromisoformat(date)
     # Форматируем в нужный вид
     formatted_date = dt.strftime("%d.%m.%Y")
     return formatted_date  # → 11.03.2024
 
-#print(get_date(input()))
 
-
-
-
-
-
-
-
-
+# print(get_date(input()))
