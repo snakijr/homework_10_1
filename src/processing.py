8fd42e5ed7aa5a11cfcd737f285b5ae419224456
Dmitriy Koluzayev <tornadomg.team@gmail.com>
2025-08-19 16:57:43 +0500

    добавлены функции и докстринги

--- src/processing.py
+++ src/processing.py
@@ -1,2 +1,23 @@
-def filter_by_state():
-    pass
+from datetime import datetime
+
+def filter_by_state(list_dict: list, state_value: str = "EXECUTED") -> list:
+    """
+    Фильтрует список словарей по значению ключа 'state'.
+
+    :param list_dict: список словарей
+    :param state_value: значение для фильтрации (по умолчанию 'EXECUTED')
+    :return: новый список словарей, где 'state' == state_value
+    """
+    return [item for item in list_dict if item.get("state") == state_value]
+
+
+
+
+def sort_by_date(list_dict: list, date_key: str = "date", reverse: bool = False) -> list:
+    '''Функция должна возвращать новый список, отсортированный по дате '''
+
+    return sorted(
+        list_dict,
+        key=lambda item: datetime.strptime(item[date_key], "%Y-%m-%d"),
+        reverse=reverse
+    )
